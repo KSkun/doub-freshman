8b25db6eaefb75967b25afe5104e1b257d5864b2
KSkun <ks@ksmeow.moe>
2021-06-13 18:41:15 +0800

    add Player.Major field

--- src/model/player.go
+++ src/model/player.go
@@ -3,6 +3,7 @@ package model
 type Player struct {
 	ID        string   `json:"id"`
 	Name      string   `json:"name"`
+	Major     string   `json:"major"`
 	Selection []string `json:"selection"`
 	Next      string   `json:"next"`
 	Flag      []Flag   `json:"flag"`
